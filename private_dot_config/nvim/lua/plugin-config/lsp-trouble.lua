e5ebdd799b35309d3257bcaff80268132b899410
Chris Pahl <sahib@online.de>
2023-10-03 16:57:24 +0200

    delete old files

--- private_dot_config/nvim/lua/plugin-config/lsp-trouble.lua
+++
@@ -1,7 +0,0 @@
-local opts = {silent = true, noremap = true}
-vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>TroubleToggle<cr>", opts)
-vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", opts)
-vim.api.nvim_set_keymap("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", opts)
-vim.api.nvim_set_keymap("n", "gD", "<cmd>TroubleToggle lsp_definitions<cr>", opts)
-vim.api.nvim_set_keymap("n", "gT", "<cmd>TroubleToggle lsp_type_definitions<cr>", opts)
-require('trouble').setup{}
