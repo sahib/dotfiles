67f2c4e2ecaa13eb04a9f48cfb90620b1de9d9b8
Chris Pahl <sahib@online.de>
2023-11-01 12:19:57 +0100

    fix: switch zsh config to prezto

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -23,7 +23,7 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<Leader>f', builtin.find_files, { desc = 'Find files' })
-        vim.keymap.set('n', '<Leader>g', builtin.grep_string, { desc = 'Find string under cursor' })
+        vim.keymap.set('n', '<Leader>g', builtin.live_grep, { desc = 'Find string under cursor' })
         vim.keymap.set('n', '<Leader>b', builtin.buffers, { desc = 'List buffers' })
         vim.keymap.set('n', '<Leader>s', builtin.lsp_document_symbols, { desc = 'List symbols' })
         vim.keymap.set('n', '<Leader>h', builtin.help_tags, { desc = 'List help tags' })
