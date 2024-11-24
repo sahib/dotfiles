98096ada88c598e7a1bec2bd2e0b4113abe0b4a5
Chris Pahl <sahib@online.de>
2024-11-24 21:47:22 +0100

    add ncspot

--- private_dot_config/waybar/modules/executable_playerctl.sh
+++ private_dot_config/waybar/modules/executable_playerctl.sh
@@ -4,34 +4,39 @@
 #       We also take advantage of the `playerctld` daemon.
 #       It just tracks the status of the last used player.
 
-status="$(playerctl -p playerctld status)"
+PLAYER=$(playerctl --list-all | grep ncspot | head -1)
+if [ -z "$PLAYER" ]; then
+  PLAYER=playerctld
+fi
+
+status="$(playerctl -p "${PLAYER}" status)"
 status_icon=""
 case "$status" in
-  Playing)
-    status_icon="▶"
-    ;;
+Playing)
+  status_icon="▶"
+  ;;
 
-  Paused)
-    status_icon="⏸"
-    ;;
+Paused)
+  status_icon="⏸"
+  ;;
 
-  Stopped)
-    status_icon="◾"
-    ;;
+Stopped)
+  status_icon="◾"
+  ;;
 
-  *)
-    # no idea what this is.
-    ;;
+*)
+  # no idea what this is.
+  ;;
 esac
 
 ellipsize() {
-	if [ "${#1}" -gt "$2" ]; then
-		echo "${1:0:$2}…"
-	fi
+  if [ "${#1}" -gt "$2" ]; then
+    echo "${1:0:$2}…"
+  fi
 
-	echo "$1"
+  echo "$1"
 }
 
-artist="$(playerctl -p playerctld metadata artist)"
-title="$(playerctl -p playerctld metadata title)"
+artist="$(playerctl -p "${PLAYER}" metadata artist)"
+title="$(playerctl -p "${PLAYER}" metadata title)"
 printf "%s %.20s - %.40s" "$status_icon" "$(ellipsize "$artist" 20)" "$(ellipsize "$title" 40)"
